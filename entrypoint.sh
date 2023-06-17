a72fdd9b2e99a590e299bcc72ea2be490ce6918b
arumino-om <102341161+arumino-om@users.noreply.github.com>
2023-06-17 11:30:29 +0900

    modify entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+echo "Change user to ${USER_ID}:${GROUP_ID}"
+groupadd -g ${GROUP_ID} dockeruser
+useradd -m -s /bin/bash -u ${USER_ID} -g {GROUP_ID} dockeruser
+
 if [ -d "/server_build" ]; then
-    mv /server_build/* /server/
+    cp /server_build/* /server/
     rm -r /server_build
+    chown ${USER_ID}:${GROUP_ID} -r /server/*
 fi
+
+exec /usr/sbin/gosu dockeruser "$@"
 cd /server/bedrock_server
 wine bedrock_server_mod.exe
